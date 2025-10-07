2591a9ca5858e15c311f8aacb62ca101a94b8f0b
FrejusAdedemi <frejusadedemi2@gmail.com>
2025-10-07 10:13:16 +0200

    feat: ajout du sytème de connexion

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php
+class Connection {
+    private $host = 'localhost';
+    private $db = 'database';
+    
+    public function connect() {
+        // Code de connexion
+        return 'Connected';
+    }
+}
